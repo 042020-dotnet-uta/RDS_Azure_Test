eb573e3f80fc333c4138009ef202b800d187e5e9
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-14 15:46:52 -0500

    Adds the pipeline model

---
+++ AzureTest/Models/Pipeline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTest.Models
+{
+    public class Pipeline
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Throughput { get; set; }
+
+    }
+}
